3d206d5e2a7a64e4940cd8e68591fdae6fa7becd
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-06-19 01:07:28 +0600

    jwt error fixing 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
